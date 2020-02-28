d02fd4814699945fa61ff3e07df750e32646c2ab
oddlyspaced <oddlyspaced1@gmail.com>
2020-02-28 20:51:47 +0530

    CheckService: Implement service to handle brightness

--- app/src/main/java/com/oddlyspaced/exb/CheckService.kt
+++ app/src/main/java/com/oddlyspaced/exb/CheckService.kt
@@ -1,12 +1,28 @@
 package com.oddlyspaced.exb
 
 import android.accessibilityservice.AccessibilityService
+import android.app.KeyguardManager
+import android.content.Context
+import android.os.Handler
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 
 class CheckService : AccessibilityService() {
+
     override fun onServiceConnected() {
         Log.e("CheckService", "Service Started")
+        setupServices()
+        check()
+    }
+
+    private lateinit var keyguardManager: KeyguardManager
+    private lateinit var brightnessManager: BrightnessManager
+    private var isLocked: Boolean = false
+    private var brightnessLocked: Int = 0 //Screen brightness just before locking
+
+    private fun setupServices() {
+        keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        brightnessManager = BrightnessManager()
     }
 
     override fun onInterrupt() {
@@ -16,4 +32,26 @@ class CheckService : AccessibilityService() {
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    private fun check() {
+        Handler().postDelayed({
+            if (keyguardManager.isDeviceLocked) {
+                if (!isLocked) {
+                    isLocked = true
+                    brightnessLocked = brightnessManager.getBrightness(applicationContext)
+                    brightnessManager.setBrightness(applicationContext, 150)
+                    Log.e("CheckService", "Screen Locked with brightness value: $brightnessLocked")
+                }
+            }
+            else {
+                if (isLocked) {
+                    isLocked = false
+                    brightnessManager.setBrightness(applicationContext, brightnessLocked)
+                    Log.e("CheckService", "Screen Unlocked, restoring brightness value: $brightnessLocked")
+                }
+            }
+            check()
+        }, 200)
+    }
+
 }
