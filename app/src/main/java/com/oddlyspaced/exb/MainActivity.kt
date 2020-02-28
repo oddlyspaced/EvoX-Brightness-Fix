9fcc5bfd404845a893262ecb4d1355b69078dc8f
oddlyspaced <oddlyspaced1@gmail.com>
2020-02-28 21:10:42 +0530

    MainActivity: Modify mainactivity

--- app/src/main/java/com/oddlyspaced/exb/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/exb/MainActivity.kt
@@ -5,22 +5,35 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        canWriteSystemSettings()
+        if (canWriteSystemSettings()) {
+            Toast.makeText(applicationContext, "Please grant secure settings permission!", Toast.LENGTH_LONG).show()
+            openAccessibilityMenu()
+        }
+        else {
+            Toast.makeText(applicationContext, "Please re-open app to grant accessibility permission.", Toast.LENGTH_LONG).show()
+        }
+        finish()
     }
 
-    private fun canWriteSystemSettings() {
+    private fun canWriteSystemSettings(): Boolean {
         if (!Settings.System.canWrite(applicationContext)) {
             val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
             intent.data = Uri.parse("package:com.oddlyspaced.exb")
             startActivity(intent)
+            return false
         }
+        return true
     }
 
+    private fun openAccessibilityMenu() {
+        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+        startActivity(intent)
+    }
 
 }
