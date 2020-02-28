03aeec90e072a45a2788732827d769ad4fda2d12
oddlyspaced <oddlyspaced1@gmail.com>
2020-02-28 20:51:00 +0530

    MainActivity: Ask for permission for modifying system settings

--- app/src/main/java/com/oddlyspaced/exb/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/exb/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.oddlyspaced.exb
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        canWriteSystemSettings()
     }
+
+    private fun canWriteSystemSettings() {
+        if (!Settings.System.canWrite(applicationContext)) {
+            val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+            intent.data = Uri.parse("package:com.oddlyspaced.exb")
+            startActivity(intent)
+        }
+    }
+
+
 }
